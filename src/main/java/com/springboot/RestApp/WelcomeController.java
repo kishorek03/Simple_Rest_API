1f5747d79a92601ee2afc312c4cf941d737d0c73
Kishore K <kishorekarthik2003@gmail.com>
2024-06-08 00:20:08 +0530

    First commit

--- src/main/java/com/springboot/RestApp/WelcomeController.java
+++ src/main/java/com/springboot/RestApp/WelcomeController.java
@@ -1,2 +1,13 @@
-package com.springboot.RestApp;public class Welcomecontroller {
+package com.springboot.RestApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome to spring boot app development";
+    }
+
 }
